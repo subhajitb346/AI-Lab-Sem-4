f809636b13d5ecfdd0efff13a4b2e88558ebf9b0
subhajitb346 <subhojitb67@gmail.com>
2026-04-20 10:04:48 +0530

    Create dfs.py

---
+++ dfs.py
@@ -0,0 +1,10 @@
+def dfs(graph, start, visited=None):
+    if visited is None:
+        visited = set()
+
+    visited.add(start)
+    print(start, end=" ")
+
+    for neighbour in graph[start]:
+        if neighbour not in visited:
+            dfs(graph, neighbour, visited)
